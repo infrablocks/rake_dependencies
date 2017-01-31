d24ba4b73b882360bae78cf6ebe11bf501d72013
Toby Clemson <tobyclemson@gmail.com>
2017-01-31 11:23:26 +0000

    Fix failing test.

--- spec/rake_dependencies_spec.rb
+++ spec/rake_dependencies_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe RakeDependencies do
   it "has a version number" do
     expect(RakeDependencies::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
